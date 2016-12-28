4f034a1ecfec12b160eed9ef5884ec6170535fa1
van1757 <van1757@gmail.com>
2016-12-28 14:34:06 +0300

    First commit

---
+++ src/server/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan'); // logger
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname + '/../../dist'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(morgan('dev'));
+
+// all other routes are handled by Angular
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname,'/../../dist/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Angular 2 Full Stack listening on port '+app.get('port'));
+});
+
+module.exports = app;
